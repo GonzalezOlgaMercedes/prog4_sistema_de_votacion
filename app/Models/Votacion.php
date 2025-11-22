5a3073a7664b54f1ad99dae313cafc675f494ac4
Gonzalez Olga Mercedes <olgamgonzalez97@gmail.com>
2025-11-22 19:42:48 -0300

    feat(votaciones): crear modelo Votacion, su migración y definir campos titulo y estado para la tabla votacions

---
+++ app/Models/Votacion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Votacion extends Model
+{
+    protected $table = 'votacions'; // Nombre de la tabla en la base de datos
+    //fillable fields
+    protected $fillable = [
+        'titulo', 
+        'estado',
+    ];
+}
