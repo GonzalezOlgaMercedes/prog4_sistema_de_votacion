6afe8e6454ce99d2fad2068f1f44da5112d5b043
Gonzalez Olga Mercedes <olgamgonzalez97@gmail.com>
2025-11-22 20:01:10 -0300

    feat(opciones): crear modelo Opcion, su migración y definir el campo opcion_disponible para la tabla opcions

---
+++ app/Models/Opcion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Opcion extends Model
+{
+    protected $table = 'opcions';
+    protected $fillable = [
+        'opcion_disponible'
+    ];
+}
