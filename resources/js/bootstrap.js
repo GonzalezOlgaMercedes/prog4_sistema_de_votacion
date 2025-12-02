259ab70cbbd2d2b56f455d8151aef1d52301981f
Gonzalez Olga Mercedes <olgamgonzalez97@gmail.com>
2025-12-02 17:01:50 -0300

    feat(navegación): habilitar el dropdown de configuración en pantallas pequeñas

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,3 +10,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import './echo';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
